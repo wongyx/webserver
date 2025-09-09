df721235836569b778e80ad433ddfdeeb18ef17a
Wong Yong Xiang <yongxiang2000@gmail.com>
2025-09-09 18:04:53 +0800

    Change UA display

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.get("/", (req, res) => {
     parser.setUA(userAgent)
     const result = parser.getResult();
 
-    const browser_info = "<p>Browser Name: " + result.browser.name + "</p>" +
-    "<p>Browser Version: " + result.browser.version + "</p>" +
-    "<p>OS Name: " + result.os.name + "</p>" +
-    "<p>OS Version: " + result.os.version + "</p>" +
-    "<p>IP: " + req.headers['x-forwarded-for'] || req.connection.remoteAddress + "</p>";
+    // const browser_info = "<p>Browser Name: " + result.browser.name + "</p>" +
+    // "<p>Browser Version: " + result.browser.version + "</p>" +
+    // "<p>OS Name: " + result.os.name + "</p>" +
+    // "<p>OS Version: " + result.os.version + "</p>" +
+    // "<p>IP: " + req.headers['x-forwarded-for'] || req.connection.remoteAddress + "</p>";
+    const browser_info = 'UA: ' + browser_info + "\nIP: " + req.headers['x-forwarded-for'] + '\n';
 
     console.log(`Logged access for rid: ${browser_info}`);
     });
